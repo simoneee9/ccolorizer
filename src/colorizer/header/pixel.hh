e236d517321b2dade127acf1f9598d251f8a663b
simoneee9 <hacajsimon@gmail.com>
2024-07-20 13:13:15 +0200

    fix color space conversion!!

--- src/colorizer/header/pixel.hh
+++ src/colorizer/header/pixel.hh
@@ -1,18 +1,20 @@
 #pragma once
 
+#include <algorithm>
 #include <array>
 #include <cstdint>
+#include <iostream>
 
 struct Pixel
 {
   Pixel(uint64_t x, uint64_t y): x(x), y(y) {}
 
-  Pixel(uint64_t x, uint64_t y, uint8_t r, uint8_t g, uint8_t b)
+  Pixel(uint64_t x, uint64_t y, uint8_t red, uint8_t gre, uint8_t blu)
   : x(x), y(y) {
     
-    double r_n = static_cast<double>(r) / 255.0;
-    double g_n = static_cast<double>(g) / 255.0;
-    double b_n = static_cast<double>(b) / 255.0;
+    double r_n = static_cast<double>(red) / 255.0;
+    double g_n = static_cast<double>(gre) / 255.0;
+    double b_n = static_cast<double>(blu) / 255.0;
     
     // actually im too lazy to figure out what is white point normalization
     // i literally worked for 8 hours today trying to make windows containers work
@@ -22,10 +24,9 @@ struct Pixel
     // double z_tris = 0.0193339 * r_n + 0.1191920 * g_n + 0.9503041 * b_n;
 
     // actually converting to YUV hehehehee
-    L =  0.257 * r + 0.504 * g + 0.098 * b + 16;
-    a = -0.148 * r - 0.291 * g + 0.439 * b + 128;
-    b =  0.439 * r - 0.368 * g - 0.071 * b + 128;
-
+    L =  0.229 * r_n + 0.587 * g_n + 0.114 * b_n + 16;
+    a = -0.147 * r_n - 0.289 * g_n + 0.436 * b_n + 128;
+    b =  0.615 * r_n - 0.515 * g_n - 0.100 * b_n + 128;
   }
 
   // int32_t L;
@@ -46,11 +47,15 @@ struct Pixel
     double a_t = a - 128;
     double b_t = b - 128;
 
-    uint8_t r = 1.164 * L_t               + 1.596 * b_t;
-    uint8_t g = 1.164 * L_t - 0.392 * a_t - 0.813 * b_t;
-    uint8_t b = 1.164 * L_t + 2.017 * a_t              ;
+    double red = (1.164 * L_t               + 1.596 * b_t) * 255 ;
+    double gre = (1.164 * L_t - 0.392 * a_t - 0.813 * b_t) * 255 ;
+    double blu = (1.164 * L_t + 2.017 * a_t              ) * 255 ;
 
-    return {r, g, b};
+    uint8_t rr = std::clamp(red, 0.0, 255.0);
+    uint8_t gg = std::clamp(gre, 0.0, 255.0);
+    uint8_t bb = std::clamp(blu, 0.0, 255.0);
+
+    return {rr, gg, bb};
   }
 
 };
